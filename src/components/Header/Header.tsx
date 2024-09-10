a71ad778dff426706686c66c809bae2ba627090f
milton.neto <milton.neto@parnamirim.rn.gov.br>
2024-09-10 15:56:20 -0300

    feat: implement header component with company logo and title

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Header.css";
+
+const Header: React.FC = () => {
+  return (
+    <header className="header">
+      <div className="header-content">
+        <img src="/logo.svg" alt="Logo da Empresa" className="logo" />
+        <h1 className="title">Gerador de Cartão de Visita</h1>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
