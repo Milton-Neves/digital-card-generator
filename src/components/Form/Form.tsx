3231c9712505cdff3b7bca895a273dfa2b8e8b94
milton.neto <milton.neto@parnamirim.rn.gov.br>
2024-09-12 15:16:19 -0300

    style: first mobile apply

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -43,67 +43,83 @@ const FormComponent: React.FC = () => {
 
   return (
     <div className="form-container">
-      <h1>Gerador de Cartão de Visita</h1>
+      <div className="test">
+        <h1>Gerador de Cartão de Visita</h1>
 
-      <p>
-        Crie grátis seu cartão de visita em passos rápidos! Você o insere no
-        Instagram e demais canais digitais.
-      </p>
-
-      <img src="/image.svg" alt="imagem" />
-
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="form-group">
-          <label htmlFor="name">Nome* </label>
-          <input id="name" {...register("name")} />
-          {errors.name && <p className="error">{errors.name.message}</p>}
+        <p>
+          Crie grátis seu cartão de visita em passos rápidos! Você o insere no
+          Instagram e demais canais digitais.
+        </p>
+      </div>
+      <div className="test2">
+        <div className="test3">
+          <img src="/assets/image.svg" alt="imagem ilustrativa" />
         </div>
+        <div className="test4">
+          <form>
+            <div className="form-group">
+              <label htmlFor="name">Nome* </label>
+              <input
+                id="name"
+                placeholder="Digite seu nome"
+                {...register("name")}
+              />
+              {errors.name && <p className="error">{errors.name.message}</p>}
+            </div>
+            <div className="form-group">
+              <label htmlFor="phone">Telefone* </label>
+              <InputMask
+                id="phone"
+                placeholder="(00) 0 0000-0000"
+                mask="(99) 9999[9]-9999"
+                {...register("phone")}
+              />
+              {errors.phone && <p className="error">{errors.phone.message}</p>}
+            </div>
+            <div className="form-group">
+              <label htmlFor="email">E-mail* </label>
+              <input
+                id="email"
+                placeholder="nome@email.com"
+                {...register("email")}
+              />
+              {errors.email && <p className="error">{errors.email.message}</p>}
+            </div>
+          </form>
 
-        <div className="form-group">
-          <label htmlFor="phone">Telefone* </label>
-          <InputMask
-            id="phone"
-            mask="(99) 9999[9]-9999"
-            {...register("phone")}
-          />
-          {errors.phone && <p className="error">{errors.phone.message}</p>}
+          <div className="info">
+            <ul>
+              <li>
+                Ao preencher o formulário, concordo * em receber comunicações de
+                acordo com meus interesses.
+              </li>
+              <li>
+                Ao informar meus dados, eu concordo com a{" "}
+                <a
+                  href="https://legal.rdstation.com/pt/privacy-policy/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Política de Privacidade
+                </a>
+              </li>
+            </ul>
+            <p>
+              * Você pode alterar suas permissões de comunicação a qualquer
+              tempo.
+            </p>
+          </div>
+          <button type="submit" className="btn-submit">
+            <span className="flex-center" aria-label="Gerar cartão grátis">
+              <p id="btn-text">GERAR CARTÃO GRÁTIS</p>
+              <img
+                src="/assets/arrow-right-btn.svg"
+                alt="seta indicando próximo passo"
+              />
+            </span>
+          </button>
         </div>
-
-        <div className="form-group">
-          <label htmlFor="email">E-mail* </label>
-          <input id="email" {...register("email")} />
-          {errors.email && <p className="error">{errors.email.message}</p>}
-        </div>
-
-        <div className="info">
-          <ul>
-            <li>
-              Ao preencher o formulário, concordo * em receber comunicações de
-              acordo com meus interesses.
-            </li>
-            <li>
-              Ao informar meus dados, eu concordo com a{" "}
-              <a
-                href="https://legal.rdstation.com/pt/privacy-policy/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Política de Privacidade
-              </a>
-            </li>
-          </ul>
-          <p>
-            * Você pode alterar suas permissões de comunicação a qualquer tempo.
-          </p>
-        </div>
-
-        <button type="submit" className="btn-submit">
-          <span className="flex-center">
-            <p id="btn-text">GERAR CARTÃO GRÁTIS</p>
-            <img src="/arrow-right-btn.svg" alt="" />
-          </span>
-        </button>
-      </form>
+      </div>
     </div>
   );
 };
