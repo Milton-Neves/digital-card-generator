0a77c41aa7f2a08c823a78568f83947b221cbcb6
milton.neto <milton.neto@parnamirim.rn.gov.br>
2024-09-13 10:33:09 -0300

    style: first mobile result

--- src/Pages/ResultPage.tsx
+++ src/Pages/ResultPage.tsx
@@ -1,14 +1,19 @@
 import React from "react";
-import { useLocation, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import "./ResultPage.css";
 
+interface BusinessCardInfo {
+  name: string;
+  phone: string;
+  email: string;
+}
+
 const ResultPage: React.FC = () => {
-  const location = useLocation();
   const navigate = useNavigate();
-  const formData = location.state as {
-    name: string;
-    email: string;
-    phone: string;
+  const cardInfo: BusinessCardInfo = {
+    name: "Nome e Sobrenome",
+    phone: "(00) 0000-0000",
+    email: "meuemail@email.com",
   };
 
   const handleBack = () => {
@@ -18,35 +23,37 @@ const ResultPage: React.FC = () => {
   return (
     <div className="result-container">
       {/* Botão para gerar outro cartão */}
-      <button className="back-button" onClick={handleBack}>
-        <span className="flex-center" aria-label="Gerar outro cartão">
-          <img src="/assets/arrow-left.svg" alt="seta indicando retorno" />
-          <p id="btn-text">Gerar outro cartão</p>
-        </span>
-      </button>
+      <div className="test1">
+        <button className="back-button" onClick={handleBack}>
+          <span className="flex-center" aria-label="Gerar outro cartão">
+            <img src="/assets/arrow-left.svg" alt="seta indicando retorno" />
+            <p id="btn-text">Gerar outro cartão</p>
+          </span>
+        </button>
 
-      {/* Cartão estilizado */}
-      <div className="card">
-        <img src="/assets/icon-logo.svg" alt="Logo" className="card-logo" />
-        <div className="card-line"></div>
-        {/* <div className="card-info">
-          <p className="card-name">{formData?.name}</p>
-          <p className="card-phone">{formData?.phone}</p>
-          <p className="card-email">{formData?.email}</p>
-        </div> */}
+        {/* Cartão estilizado */}
+        <div className="card">
+          <img src="/assets/icon-logo.svg" alt="Logo" className="card-logo" />
+          <div className="card-line"></div>
+          <div className="card-info">
+            <p className="card-name">{cardInfo.name}</p>
+            <p className="card-phone">{cardInfo.phone}</p>
+            <p className="card-email">{cardInfo.email}</p>
+          </div>
+        </div>
+
+        {/* Botão de download desabilitado */}
+
+        <button type="submit" className="download-button">
+          <span className="flex-center" aria-label="Gerar cartão grátis">
+            <img
+              src="/assets/arrow-down.svg"
+              alt="seta indicando próximo passo"
+            />
+            <p id="btn-text">BAIXAR CARTÃO</p>
+          </span>
+        </button>
       </div>
-
-      {/* Botão de download desabilitado */}
-     
-      <button type="submit" className="download-button">
-        <span className="flex-center" aria-label="Gerar cartão grátis">
-          <img
-            src="/assets/arrow-down.svg"
-            alt="seta indicando próximo passo"
-          />
-          <p id="btn-text">BAIXAR CARTÃO</p>
-        </span>
-      </button>
 
       {/* Link para o teste grátis no RD Station */}
       <a
