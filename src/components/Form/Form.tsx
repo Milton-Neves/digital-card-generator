5ddc8c4440872b81aea561187ce7b80b21e21069
Milton-Neves <milzonzneves@gmail.com>
2024-09-14 14:07:29 -0300

    sup: error analised

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -20,15 +20,15 @@ const schema = yup.object().shape({
   phone: yup
     .string()
     .required("Telefone é obrigatório")
-    .test('len', 'Número de telefone inválido', (value) => {
-      const phone = value ? value.replace(/\D/g, '') : '';
+    .test("len", "Número de telefone inválido", (value) => {
+      const phone = value ? value.replace(/\D/g, "") : "";
       return phone.length === 10 || phone.length === 11;
-    })
+    }),
 });
 
 // Função para remover a máscara do telefone
 const removeMask = (value: string) => {
-  return value.replace(/\D/g, ''); // Remove todos os caracteres que não são números
+  return value.replace(/\D/g, ""); // Remove todos os caracteres que não são números
 };
 
 type FormData = {
@@ -38,9 +38,15 @@ type FormData = {
 };
 
 const FormComponent: React.FC = () => {
-  const { register, handleSubmit, formState: { errors, isValid }, control, reset } = useForm<FormData>({
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+    control,
+    reset,
+  } = useForm<FormData>({
     resolver: yupResolver(schema),
-    mode: "onChange" // Recalcula a validação a cada alteração
+    mode: "onChange", // Recalcula a validação a cada alteração
   });
 
   const navigate = useNavigate();
@@ -52,16 +58,13 @@ const FormComponent: React.FC = () => {
         ...data,
         phone: removeMask(data.phone),
       };
-      console.log("Dados sem máscara:", dataWithoutMask);
 
       // Salvar os dados no localStorage
       localStorage.setItem("businessCardData", JSON.stringify(dataWithoutMask));
 
       // Navegar para a página de resultado
       navigate("/resultado");
-    } catch (error) {
-      console.log('Erro ao enviar o formulário:', error);
-    }
+    } catch (error) {}
   };
 
   return (
